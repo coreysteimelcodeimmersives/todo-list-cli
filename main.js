39a3aab7bcdd53fd611fe05d9c785689929dd894
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-02-22 13:32:05 -0700

    now i can display how many incomplete items the user has

--- main.js
+++ main.js
@@ -34,20 +34,22 @@ function userAction(str){
 }
 
 function completeAnItem(){
-    checkIfAllItemsAreComplete();
     console.log('\n');
     console.log('==============================================');
     console.log('\n');
     console.log('~ Completing a to-do item ~');
     displayToDoList();
+    checkIfAllItemsAreComplete();
     console.log('Which to-do item would you like to complete?')
     itemNumberString = prompt('> ');
     itemNumber = Number(itemNumberString);
     if (itemNumber > toDoArray.length){
         exceptionItemDoesNotExist()
-    } else if (itemNumber > 0 && itemNumber <= toDoArray.length){
+    } else if (itemNumber > 0 && itemNumber <= toDoArray.length && toDoArray[itemNumber-1][1] === false){
         writeItemComplete(itemNumber);
-    } else if (itemNumber > toDoArray.length){
+    } else if (toDoArray[itemNumber-1] === true){
+        exceptionItemAlreadyComplete();
+    }else if (itemNumber > toDoArray.length){
         exceptionItemDoesNotExist();
     } else {
         exceptionInvalidCompletionEntry();
@@ -57,23 +59,45 @@ function completeAnItem(){
     selectAnAction();
 }
 
+function exceptionItemAlreadyComplete(){
+    console.log('\n');
+    console.log('==============================================');
+    console.log('\n');
+    console.log('Error: You have already completed that item');
+    console.log('Please choose an incomplete item from your to-do list')
+    return completeAnItem();
+}
+
 function writeItemComplete(num){
     toDoArray[num-1][1] = true;
 }
 
 function checkIfAllItemsAreComplete(){
+    let incomplete = true;
     for(let i = 0; i < toDoArray.length; i++){
         if (toDoArray[i][1] === false){
-            return;
-        } else {
-            console.log('Congratulations, you have completed all the items on your to-do list!');
-            console.log('You can now add a new item to your to-do list');
-            console.log('\n');
-            console.log('==============================================');
-            console.log('\n');
-            return selectAnAction();
+            incomplete = false;
         }
+        //     break;
+        // } else if (i === toDoArray.length-1 && toDoArray[i][1] === true) {
+        //     congrats(); 
+        // } else {
+        //     congrats();
+        // }
     }
+    if (incomplete === true){
+        congrats();
+    }
+    return;
+}
+
+function congrats(){
+    console.log('Congratulations, you have completed all the items on your to-do list!');
+    console.log('You can now add a new item to your to-do list');
+    console.log('\n');
+    console.log('==============================================');
+    console.log('\n');
+    return selectAnAction();
 }
 
 function exceptionItemDoesNotExist(){
@@ -118,11 +142,19 @@ function addItemToDoList(str){
     return selectAnAction();
 }
 
+function incompleteItemsCount(){
+    let completeItems = 0;
+    for (i = 0; i < toDoArray.length; i++){
+        if (toDoArray[i][1] === true){
+            completeItems += 1;
+        }
+    }
+    return completeItems;
+}
+
 function displayToDoList() {
-    console.log('\n');
-    console.log('==============================================');
-    console.log('\n');
-    console.log('You have ' + toDoArray.length + ' to-do item(s).');
+    let completeItems = toDoArray.length - incompleteItemsCount();
+    console.log('You have ' + completeItems + ' incomplete to-do item(s).');
     for (i = 0; i < toDoArray.length; i++){
         if (toDoArray[i][1] === true){
             isComplete = 'complete';
